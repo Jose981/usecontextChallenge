c547ace74c5e50e5fd7be5213c9e1a9fc4803db7
Pepe MR <pepemartos98@gmail.com>
2026-03-26 12:10:20 +0100

    entrega ejercicio context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'; // Archivo de estilos CSS
 
-const App = () => {
+import { useContext } from "react";
+import { ThemeProvider, ThemeContext } from "./themes/ThemeContext";
+import RoutesApp from "./routes/RoutesApp";
+
+const AppContent = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-<></>
+    <section className={`App ${theme}`}>
+      <RoutesApp />
+    </section>
   );
 };
+
+function App() {
+  return (
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
+  );
+}
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { ThemeContext } from "../themes/ThemeContext";
+
+const Button = () => {
+    const {toggleTheme} = useContext(ThemeContext);
+    return (
+        <button onClick={toggleTheme}>
+            Cambiar Tema
+        </button>
+    );
+};
+
+export default Button;
